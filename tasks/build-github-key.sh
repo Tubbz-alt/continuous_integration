78c96823d93e7b854a1b46fc2fa4e2bbc523a34b
Ellery Crane <ellery@pwnieexpress.com>
2017-05-24 15:32:01 -0400

    copy please

--- tasks/build-github-key.sh
+++ tasks/build-github-key.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 OUTPUT_DIR=${PWD}/built-github-key
+#mkdir -p ${OUTPUT_DIR}
 echo "Copying app contents..."
-mv ./cloud-ui-client-app ${OUTPUT_DIR}
+cp -rT ./cloud-ui-client-app ${OUTPUT_DIR}
 echo "Copying github key.."
 echo "${GITHUB_PRIVATE_KEY}" >> ${OUTPUT_DIR}/id_rsa
 echo "Done"
