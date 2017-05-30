075e73c7910f70d0fdd70da5f19b76e779aa46d9
Ellery Crane <ellery@pwnieexpress.com>
2017-05-30 14:09:36 -0400

    Latest changes to pipelines.

--- tasks/build-github-key.sh
+++ tasks/build-github-key.sh
@@ -2,6 +2,7 @@
 OUTPUT_DIR=${PWD}/built-github-key
 mkdir -p ${OUTPUT_DIR}
 echo "Copying app contents..."
+# note the trailing period after the app name; this is important to ensure that hidden files also get copied
 cp -r ./cloud-ui-client-app/. ${OUTPUT_DIR}
 echo "Copying github key.."
 echo "${GITHUB_PRIVATE_KEY}" >> ${OUTPUT_DIR}/id_rsa
