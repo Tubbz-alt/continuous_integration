73b27423e78bac56b755efcfc4ab88864cd7fb99
Ellery Crane <ellery@pwnieexpress.com>
2017-07-07 12:29:35 -0400

    Updated task to work with arbitary app names.

--- tasks/build-github-key.sh
+++ tasks/build-github-key.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 OUTPUT_DIR=${PWD}/built-github-key
+# note the trailing period after the app name; this is important to ensure that hidden files also get copied
+INPUT_DIR=./${APP_RESOURCE_NAME}/.
 mkdir -p ${OUTPUT_DIR}
 echo "Copying app contents..."
-# note the trailing period after the app name; this is important to ensure that hidden files also get copied
-cp -r ./cloud-ui-client-app/. ${OUTPUT_DIR}
+cp -r ${INPUT_DIR} ${OUTPUT_DIR}
 echo "Copying github key.."
 echo "${GITHUB_PRIVATE_KEY}" >> ${OUTPUT_DIR}/id_rsa
 echo "Done"
